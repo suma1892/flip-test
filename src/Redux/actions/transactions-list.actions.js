d2f7c2fad24c507a351562a21f46b61ced494059
Suma <sumaalbaroh1892@gmail.com>
2021-06-18 13:54:58 +0700

    fix non redux

--- src/Redux/actions/transactions-list.actions.js
+++ src/Redux/actions/transactions-list.actions.js
@@ -1,30 +1,61 @@
 
-import { transactionListConstant as tc} from '../constant/transactions-list.constant';
+import { transactionListConstant as tc } from '../constant/transactions-list.constant';
 import axios from 'axios';
-export function transactionListActions(data){
-    return (dispatch) =>{
-        dispatch ({
-            type: tc.LOADING
-        })
-        let data = {
-            method: 'get',
-            url: `https://nextar.flip.id/frontend-test`, 
-          }
-       return axios(data)
-            .then((res) => {
-                    var obj = res.data;
-                    var result = Object.keys(obj).map((key) => [obj[key]]);
-                    dispatch ({
-                        type: tc.SUCCESS,
-                        payload: result
-                    });
-                }
-            )
-            .catch(error => {
-                return dispatch({
-                    type   : tc.ERROR,
-                    payload: error
-                });
-            });
+
+export const transactionListActions = (params, body) => {
+
+    let data = {
+        method: 'get',
+        url: `https://nextar.flip.id/frontend-test`,
+    }
+
+    // let data = {
+    //     method: 'post',
+    //     url: `https://nextar.flip.id/frontend-test`,
+    //     params: params, // UNTUK METHOD POST PARAMS,
+    //     data: body // UNTUK METHOD POST BODY PAKE NYA data
+    // }
+
+    return axios(data)
+        .then((res) => {
+            var obj = res.data;
+            var result = Object.keys(obj).map((key) => [obj[key]]);
+            return result;
         }
+        )
+        .catch(error => {
+            return dispatch({
+                type: tc.ERROR,
+                payload: error
+            });
+        });
 }
+
+// export function transactionListActions(data){
+//     return (dispatch) =>{
+//         dispatch ({
+//             type: tc.LOADING
+//         })
+//         let data = {
+//             method: 'get',
+//             url: `https://nextar.flip.id/frontend-test`, 
+//           }
+//        return axios(data)
+//             .then((res) => {
+//                     var obj = res.data;
+//                     var result = Object.keys(obj).map((key) => [obj[key]]);
+//                     dispatch ({
+//                         type: tc.SUCCESS,
+//                         payload: result
+//                     });
+//                 }
+//             )
+//             .catch(error => {
+//                 return dispatch({
+//                     type   : tc.ERROR,
+//                     payload: error
+//                 });
+//             });
+//         }
+// }
+
